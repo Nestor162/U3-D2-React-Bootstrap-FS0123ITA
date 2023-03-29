5b30c537349cb10010c8b6897ef39968c5835737
Nestor162 <nestorcicardini@gmail.com>
2023-03-29 14:47:58 +0200

    Creata nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import MyNav from "./components/MyNav";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyNav />
+    </div>
+  );
 }
 
 export default App;
